f489d3ca643fe816c5616578a0a67048af78fd41
Harshal Patil <patilharshal39730@gmail.com>
2026-07-14 08:45:46 +0530

    Update Logo.jsx

--- src/components/shared/Logo.jsx
+++ src/components/shared/Logo.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import logoImg from '../../assets/logonew.png'
 
-export default function Logo({ size = 22, rounded = true }) {
+export default function Logo({ size = 22 }) {
   return (
     <img
       src={logoImg}
       alt="CodeCanvas"
-      width={size}
-      height={size}
-      style={{ borderRadius: rounded ? size * 0.28 : 0, display: 'block', flexShrink: 0 }}
+      style={{ height: size, width: 'auto', display: 'block', flexShrink: 0 }}
     />
   )
 }
